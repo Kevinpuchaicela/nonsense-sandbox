4e9da95cf8326978a7ce83cd2a9be6b106b51c7a
KevinPuchaicela <kpuchaicela@cifpfbmoll.eu>
2020-12-22 16:02:03 +0100

    feat: +1

--- src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
+++ src/main/java/eu/bmoll/daw/programming/NonsenseApp.java
@@ -1,16 +1,21 @@
 package eu.bmoll.daw.programming;
 
+import eu.bmoll.daw.programming.helper.ConsoleUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
 
 public class NonsenseApp {
+    private static String EXIT_KEY = "9";
+
     public static void main(String[] args) {
-        boolean aux = StringUtils.equals("a", "A");
-        System.out.println("Hello World " + aux);
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println("You entered string " +s);
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+        boolean isExit = false;
+        while (!isExit) {
+            String s = consoleUtils.getUserInput();
+            System.out.println("Your intered is: " + s);
+            isExit = StringUtils.equals(s, EXIT_KEY);
+        }
         System.out.println("END");
     }
 }
